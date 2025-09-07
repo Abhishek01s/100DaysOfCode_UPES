6d2be59ba0103cf8d57acdfb8c41758e80871f1f
Abhishek01s <69300019+Abhishek01s@users.noreply.github.com>
2025-09-07 13:55:44 +0530

    Create 005.c

---
+++ Day 3/005.c
@@ -0,0 +1,10 @@
+// Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+
+int main(){
+  int cel;
+  scanf("%d", &cel);
+  printf("Fahrenheit=%d\n", cel*9/5+32);
+  return 0;
+}
