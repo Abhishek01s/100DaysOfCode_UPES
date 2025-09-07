3d1b97e6437bd1f380caa2e25f19a86343cade76
Abhishek01s <69300019+Abhishek01s@users.noreply.github.com>
2025-09-07 13:58:35 +0530

    Create 006.c

---
+++ Day 3/006.c
@@ -0,0 +1,13 @@
+// Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+
+int main(){
+  int a, b, c;
+  scanf("%d %d", &a, &b);
+  c = a;
+  a = b;
+  b = c;
+  printf("After swap: %d %d\n", a, b);
+  return 0;
+}
