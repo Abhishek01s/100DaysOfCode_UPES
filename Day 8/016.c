0cd86408a62f7db1aa2396846d53db4dedbd3b00
Abhishek01s <69300019+Abhishek01s@users.noreply.github.com>
2025-09-07 18:59:34 +0530

    Create 016.c

---
+++ Day 8/016.c
@@ -0,0 +1,15 @@
+// Write a program to input three numbers and find the largest among them using if–else.
+
+#include <stdio.h>
+
+int main(){
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if(a>b && b>c)
+    printf("Largest is %d\n", a);
+  else if(b>c)
+    printf("Largest is %d\n", b);
+  else
+    printf("Largest is %d\n", c);
+  return 0;
+}
